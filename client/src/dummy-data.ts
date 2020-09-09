774fc719df23d2710141fc5eb634697631295b6e
Sila <sila@Silas-MacBook-Pro.local>
2020-09-09 14:43:01 +0200

    untracked dist

--- client/src/dummy-data.ts
+++ client/src/dummy-data.ts
@@ -1,27 +1,27 @@
 import { v4 as uuidv4 } from 'uuid';
-import {Todo} from 'types'
+import { Todo } from 'models/types'
 
 export const todoList: Todo[] = [
   {
-    id:uuidv4(),
+    id: uuidv4(),
     title: 'Study',
     description: 'Finish TS tutorials',
     completed: false,
   },
   {
-    id:uuidv4(),
+    id: uuidv4(),
     title: 'Exercise',
     description: 'Go to the park and run for 20 minutes',
     completed: true,
   },
   {
-    id:uuidv4(),
+    id: uuidv4(),
     title: 'Clean the house',
     description: 'Wipe the floors, wash the dishes, do laundry',
     completed: false,
   },
   {
-    id:uuidv4(),
+    id: uuidv4(),
     title: 'Meet your friend',
     description: "Be  in Amsterdam Oost at 19 o'clock",
     completed: false,
